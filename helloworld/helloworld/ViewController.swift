08a2809e0d9ef6796c14f55062618e08b9a0a659
Xu Norman <norman_xu@xus-macbook-air.local>
2018-07-08 21:26:37 +0800

    version 1

---
+++ helloworld/helloworld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  helloworld
+//
+//  Created by Xu Norman on 2018/7/8.
+//  Copyright © 2018 Xu Norman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
